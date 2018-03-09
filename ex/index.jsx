2a1952c1b5a985318b78ac92bceb7960ae621f27
matheus-lima92 <lima.eca@gmail.com>
2018-03-09 01:53:36 -0300

    integration with first component

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Component from './component'
 
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+//ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+
+ReactDOM.render(<Component />, document.getElementById('app'))
