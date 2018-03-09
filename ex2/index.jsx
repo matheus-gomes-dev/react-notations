3e8ab5bf038b0bcb9939a71d3c2187ffe82df6a2
matheus-lima92 <lima.eca@gmail.com>
2018-03-09 03:03:19 -0300

    example rendering with props as parameter

---
+++ ex2/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Component from './component'
+
+//ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+
+ReactDOM.render(<Component value='Show!'/>, document.getElementById('app'))
